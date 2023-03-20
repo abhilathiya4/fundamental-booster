bae313a3f20e81c371c4fc91ca94e9d4bb4bd88c
abhilathiya4 <123169803+abhilathiya4@users.noreply.github.com>
2023-03-20 13:36:05 +0530

    Add files via upload

---
+++ fb_5.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+
+
+using namespace std;
+int main(){
+	
+	int arr[5];
+	int n , i ;
+	cout<<"enter 5 value"<<endl;
+	
+	for(i=0 ; i<=4 ;i++){
+		
+	cin>>n;
+	
+	arr[i]=n;
+	
+	}
+	
+	for(i=0 ; i<=4 ;i++){
+	
+	cout<<arr[i];
+	cout<<" ";
+	
+	}
+	cout<<endl;
+	
+	int cube;
+	
+	for(i=0 ; i<=4 ; i++){
+		
+		n=arr[i];
+		cube = n*n*n;
+		
+		cout<<"Cube of "<<arr[i]<<" : "<<cube<<endl;
+	}
+	
+
+	
+	/*
+	for(i=0 ; i<=4 ; i++){
+		
+		
+		
+	}
+	
+	*/
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
